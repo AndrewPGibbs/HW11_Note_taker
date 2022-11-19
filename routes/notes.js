98304b73b7a0bd88a641bf7c86885a023eb18d71
Andrew PG <andrewpgibbs71@gmail.com>
2022-11-19 15:53:43 -0500

    added get route

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,21 @@
-const notes = require('express').Router();
+const app = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
+
+
+// GET route
+app.get('/', (req, res) =>{
+    readFromFile('/db/db.json')
+    .then((response) => res.json(JSON.parse(response)));
+});
+
+
+//POST route
+app.post('/', (req, res) => {
+    const {title}
+})
+
+//DELETE route
+
+
 
 module.exports = router;
